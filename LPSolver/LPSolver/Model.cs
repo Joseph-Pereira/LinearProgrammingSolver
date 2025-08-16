9b0696c49891f0d81c46dd0651ed2a6643fa481f
Markus du Plessis <markusdup007@gmail.com>
2025-08-16 19:13:41 +0200

    Added class to store input
     added the model class that declares a few objects to store the input data

---
+++ LPSolver/LPSolver/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPSolver
+{
+   public class LinearStorage
+    {
+        public bool IsMaximization { get; set; }
+        public double[] ObjectiveCoefficients { get; set; }
+        public List<Constraint> Constraints { get; set; }
+        public string[]  SignRestriction { get; set; }
+        
+        public LinearStorage()
+        {
+            Constraints = new List<Constraint>();
+        }
+
+    }
+
+    public class Constraint
+    {
+        public double[] Coefficients { get; set; }
+        public string Sign {  get; set; }
+        public double RHS { get; set; }
+
+    }
+}
